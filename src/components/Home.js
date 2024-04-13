3ac0d46bcd4fff4e88f8f8b53a281f8cf9c1ffbc
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-04-13 09:49:54 +0545

    scroll and change color

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import { home } from '../data'
 
 const Home = () => {
   return (
-    <>
+    <div className='bg-red-500'>
     <Navbar/>
-    <div className=' bg-red-500 flex flex-col justify-center items-center h-screen  w-screen text-2xl font-fontRoboto '>
+    <div className=' bg-red-500 flex flex-col justify-center items-center h-[70rem]  w-screen text-2xl font-fontRoboto '>
 
 <p className=' text-3xl '>{home.title}</p>
 <p>{home.content}</p>
   </div>
-  </>
+  </div>
   )
 }
 export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,27 @@
+import { useState } from "react"
 
 
 const Navbar = () => {
    
-     
-   
+     const[scroll,setScroll]=useState(false)
+  
+     const change=()=>{
+      if(window.scrollY>=100)
+      {
+        setScroll(true);
+       
+      }
+      else{
+        setScroll(false)
+      }
+     }
+ 
+   window.addEventListener('scroll',change)
   return (
 
 
-    <div className=' flex flex-row px-[10rem] pt-2 h-[4rem] justify-center text-2xl  text-white bg-red-500 '>
-        <p className='flex-grow font-Inter text-2xl'>Ragat Nepal</p>
+    <div className={ ` ${ scroll ? 'sticky top-0  bg-red-900  ': " "} flex flex-row w-[100%] h-[4rem] justify-between items-center text-2xl  text-white   px-[4rem] ` }>
+        <p className=' font-Inter text-2xl'>Ragat Nepal</p>
         <span className='flex flex-row float-end gap-[4rem] font-fontRoboto"'>
         <p className="font-fontRoboto">Home</p>
         <p>Find donor</p>
