7c8b98456417efcc6a1fcd841b10653be9def24a
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-04-24 17:50:55 +0545

    some animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,27 +4,37 @@ import Button from './Button'
 import Navbar from './Navbar'
 import About from './About'
 import Footer from './Footer'
+import Lottie from "lottie-react";
 
+import Bloodanimation from "../assets/Bloodanimation.json"
 const Home = () => {
+const style={
+width: "400px",
 
-  return (<>
+}
+
+  return (
+  <>
  
-    <div className="  h-screen bg-cover   text-[#B4BDCB] " id="/">
+    <div className="  h-screen bg-cover   text-[#B4BDCB]  " id="/">
    
-      <div className="  bg-[#08111C]   h-full  ">
+      <div className="  bg-[#08111C]   h-full flex justify-evenly  ">
       
-         <div className="flex   flex-col  h-full justify-center items-center  text-2xl font-roberto    ">
-         <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight  mb-5">
+         <div className="flex   flex-col  h-full justify-center items-center  text-2xl font-roberto float-start   ">
+         <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight  mb-5 animate__animated  animate__bounceInDown ">
         {home.title}
       </h1>
-          <div className="font-semibold text-3xl mb-5">{home.sub}</div>
-          <div className="text-xl mb-[2rem] ">{home.content}</div>
+          <div className="font-semibold text-3xl mb-5 animate__animated  animate__bounceInLeft  ">{home.sub}</div>
+          <div className="text-xl mb-[2rem] animate__animated  animate__bounceInUp ">{home.content}</div>
           <div className=' flex flex-row gap-[3rem]'>
         {/* <Button content={ {title:"Signup"}}  /> */}
         {/* <Button content={{title:"Login",bg:"#0D273F"}} /> */}
+       
         </div>
         </div>
-
+        <div className="float-end flex items-center">
+        <Lottie animationData={Bloodanimation} loop={true}  style={style}>hello</Lottie>
+        </div>
       </div>
   
      </div>
