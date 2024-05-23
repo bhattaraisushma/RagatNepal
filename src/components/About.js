93ca3508ffeabaf26920da78d4b814bc2ad68567
bhattaraisushma <bhattaraisushma260@gmail.com>
2024-05-23 20:44:41 +0545

    responsive ness

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { aboutdata } from '../data/Data'
 const About = () => {
   return (
-    <div className=' h-[43rem] w-full  flex  flex-col  pt-[7rem] items-center gap-4   bg-[#B4BDCB]  text-[#0D273F]' id='/about'>
+    <div className=' md:h-[43rem] h-fit w-full  flex  flex-col  pt-[7rem] items-center gap-4   bg-[#B4BDCB]  text-[#0D273F]' id='/about'>
        <h1 className='font-popins text-4xl '>About Ragat Nepal</h1>
        <p>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ __</p>
             <h2 className=' font-popins w-[18rem] h-fit text-center text-xl'>Lorem adipisicing elit. Similique, amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, adipisci! 
             </h2>
-          <div className='grid grid-cols-3 gap-3  mt-4'>
+          <div className='md:grid md:grid-cols-3  flex flex-col gap-3  mt-4'>
            
          {
           
@@ -17,7 +17,7 @@ const About = () => {
                 return(
 
              
-                <div className=' h-fit font-popins w-[20rem] border-solid   m-2  p-4 rounded-3xl  border hover:border-[#810000]  shadow-[#810000] hover:shadow-2xl'>
+                <div className=' h-fit font-popins w-[20rem] border-solid   m-2  p-4 rounded-3xl  border hover:border-[#810000]  shadow-[#810000] hover:shadow-2xl  '>
        
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, earum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quasi ad asperiores recusandae vitae itaque corporis alias maxime commodi eveniet.   
                    </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,21 @@ const Footer = () => {
     <div>
       <footer className="footer p-10   bg-[#0E273F]  static text-[#B4BDCB]">
   <nav>
-    <h6 className="footer-title">lorem</h6> 
+    <h6 className="footer-title font-bold">lorem</h6> 
     <a className="link link-hover">Lorem, ipsum dolor.</a>
     <a className="link link-hover">Lorem, ipsum.</a>
     <a className="link link-hover">lorem</a>
     <a className="link link-hover">Lorem</a>
   </nav> 
   <nav>
-    <h6 className="footer-title">Company</h6> 
+    <h6 className="footer-title font-bold">Company</h6> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
     <a className="link link-hover">Jobs</a>
     <a className="link link-hover">Press kit</a>
   </nav> 
   <nav>
-    <h6 className="footer-title">Legal</h6> 
+    <h6 className="footer-title font-bold">Legal</h6> 
     <a className="link link-hover">Terms of use</a>
     <a className="link link-hover">Privacy policy</a>
     <a className="link link-hover">Cookie policy</a>
